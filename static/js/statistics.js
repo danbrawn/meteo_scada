d554427d4f56afc6cf3e21798ade083a787e5ecd
danbrawn <56113218+danbrawn@users.noreply.github.com>
2025-08-22 00:50:40 +0300

    Narrow statistics dashboard card

--- static/js/statistics.js
+++ static/js/statistics.js
@@ -1,8 +1,27 @@
-$(document).ready(function() {
-  function renderStats() {
-    $('#stats-today').html('<p>Температура: 10°C - 20°C</p>');
-    $('#stats-month').html('<p>Макс температура: 25°C</p>');
-    $('#stats-year').html('<p>Годишен дъжд: 100 mm</p>');
+$(document).ready(function () {
+  function listToHtml(items) {
+    return (
+      '<ul class="stats-list">' +
+      items
+        .map(
+          item =>
+            `<li class="stats-item"><span class="stats-label">${item.label}</span>` +
+            `<span class="stats-value">${item.value}</span></li>`
+        )
+        .join('') +
+      '</ul>'
+    );
   }
-  renderStats();
+
+  fetch('/statistics_data')
+    .then(response => response.json())
+    .then(data => {
+      $('#stats-today').html(listToHtml(data.today || []));
+      $('#stats-month').html(listToHtml(data.month || []));
+      $('#stats-year').html(listToHtml(data.year || []));
+      $('#stats-alltime').html(listToHtml(data.all || []));
+    })
+    .catch(err => {
+      console.error('Error loading statistics', err);
+    });
 });
