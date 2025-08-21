86b573c6dc17e8214d603dcd9d695cceb4ed9ea1
danbrawn <56113218+danbrawn@users.noreply.github.com>
2025-08-21 23:38:00 +0300

    fix: parse numeric data for 24h graph

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,24 +1,63 @@
 $(document).ready(function() {
   function renderGraphs() {
-    const x = [1,2,3,4,5];
-    Plotly.newPlot('graph-temp-hum', [
-      {x: x, y: [10,12,14,13,11], name: 'T_AIR', type: 'scatter'},
-      {x: x, y: [60,58,65,62,61], name: 'REL_HUM', type: 'scatter'}
-    ]);
-    Plotly.newPlot('graph-pressure', [
-      {x: x, y: [950,952,951,953,954], name: 'P_ABS', type: 'scatter'},
-      {x: x, y: [930,932,931,933,934], name: 'P_REL', type: 'scatter'}
-    ]);
-    Plotly.newPlot('graph-wind', [
-      {x: x, y: [5,7,6,8,5], name: 'WIND_SPEED_1', type: 'scatter'},
-      {x: x, y: [3,4,5,4,3], name: 'WIND_SPEED_2', type: 'scatter'}
-    ]);
-    Plotly.newPlot('graph-rain', [
-      {x: x, y: [0,1,0,2,0], name: 'RAIN_MINUTE', type: 'bar'}
-    ]);
-    Plotly.newPlot('graph-radiation', [
-      {x: x, y: [100,200,150,250,300], name: 'RADIATION', type: 'scatter'}
-    ]);
+    const period = $('#period-select').val();
+    $.getJSON(`/graph_data?period=${period}`, function(data) {
+      const x = data.DateRef.map(d => new Date(d));
+      const tickSettings = {
+        '24h': { dtick: 3600000, tickformat: '%H:%M' },
+        '30d': { dtick: 86400000, tickformat: '%d.%m' },
+        '365d': { dtick: 'M1', tickformat: '%b' }
+      };
+      const baseLayout = { xaxis: tickSettings[period] };
+      const config = { responsive: true };
+
+      const plots = [
+        {
+          id: 'graph-temp-hum',
+          data: [
+            { x, y: data.T_AIR, name: 'T_AIR', type: 'scatter' },
+            { x, y: data.REL_HUM, name: 'REL_HUM', type: 'scatter' }
+          ],
+          title: 'Температура и Влажност'
+        },
+        {
+          id: 'graph-pressure',
+          data: [
+            { x, y: data.P_ABS, name: 'P_ABS', type: 'scatter' },
+            { x, y: data.P_REL, name: 'P_REL', type: 'scatter' }
+          ],
+          title: 'Налягане'
+        },
+        {
+          id: 'graph-wind',
+          data: [
+            { x, y: data.WIND_SPEED_1, name: 'WIND_SPEED_1', type: 'scatter' },
+            { x, y: data.WIND_SPEED_2, name: 'WIND_SPEED_2', type: 'scatter' }
+          ],
+          title: 'Вятър'
+        },
+        {
+          id: 'graph-rain',
+          data: [
+            { x, y: data.RAIN_MINUTE, name: 'RAIN_MINUTE', type: 'bar' }
+          ],
+          title: 'Дъжд'
+        },
+        {
+          id: 'graph-radiation',
+          data: [
+            { x, y: data.RADIATION, name: 'RADIATION', type: 'scatter' }
+          ],
+          title: 'Радиация'
+        }
+      ];
+
+      plots.forEach(plot => {
+        Plotly.purge(plot.id);
+        Plotly.newPlot(plot.id, plot.data, { ...baseLayout, title: plot.title }, config);
+        Plotly.relayout(plot.id, { margin: { l: 80, r: 80, t: 40, b: 40 } });
+      });
+    });
   }
   $('#period-select').change(renderGraphs);
   renderGraphs();
